494b24b219d3b98a73f675b61eb5f6ccefc0622b
Supun <Supuntharakaxz@gamil.com>
2024-10-04 21:54:57 +0530

    Hero section v_02

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,25 @@
+import { HERO_CONTENT } from "../constants"
+import { HERO_POSITION } from "../constants"
+import dp from "../assets/kevinRushProfile.jpg"
+
 function Hero() {
     return (
         <div className="border-b border-white pb-4 lg:mb-35">
-            <p>Text_01 Text_01 Text_01 Text_01 Text_01</p>
-            <p>Text_02 Text_02 Text_02 Text_02 Text_02</p>
-            <p>Text_03 Text_03 Text_03 Text_03 Text_3</p>
-            <p>Text_04 Text_04 Text_04 Text_04 Text_04</p>
+            <div className="flex flex-wrap">
+                <div className="w-full lg:w-1/2">
+                    <div className="flex flex-col items-center lg:items-start">
+                        <h1 className="pb-16 text-6xl font-thin tracking-widest lg:text-8xl lg:mt-16">Supun Tharaka</h1>
+                    </div>
+                    <span className="bg-gradient-to-r
+                    from-purple-400 via-white to-cyan-300 bg-clip-text text-transparent text-4xl">
+                        {HERO_POSITION}
+                    </span>
+                    <p className="my-2 max-w-xl font-light text-justify tracking-tighter">
+                        {HERO_CONTENT}
+                    </p>
+                </div>
+                <img className="w-full lg:w-1/2 lg:px-20" src={dp} alt="" />
+            </div>
         </div>
     )
 }
