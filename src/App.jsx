c62c32c50bee0533a4eea3a8d0fd7ffbdbaaf0bd
Supun <Supuntharakaxz@gamil.com>
2024-10-04 22:55:32 +0530

    About section

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import AboutMe from "./components/AboutMe"
 
 function App() {
   return (
-    <div className="overflow-x-hidden text-white antialiased selection:bg-purple-600 selection:text-white relative">
+    <div className="overflow-x-hidden text-white antialiased selection:bg-purple-900 selection:text-cyan-200 relative">
       {/* Background */}
       <div className="fixed top-0 left-0 -z-10 h-screen w-full">
         <div className="absolute inset-0 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,6 +1,24 @@
+import AboutImg from "../assets/about.jpg"
+import { ABOUT_TEXT } from "../constants"
+
 function AboutMe() {
     return (
-        0
+        <div className="border-b border-white pb-4">
+            <h2 className="my-20 text-center text-4xl">About <span className="bg-gradient-to-r from-purple-400 via-white to-cyan-300 bg-clip-text text-transparent" >Me</span>
+            </h2>
+
+            <div className="flex flex-wrap">
+                <div className="w-full lg:w-1/2 lg:p-8">
+                    <div className="flex items-center justify-center">
+                        <img className="rounded-2xl" src={AboutImg} alt="About Image" />
+                    </div>
+
+                </div>
+                <div className="w-full lg:w-1/2 lg:p-8">
+                    <div className="flex justify-center lg:justify-start" ><p className="my-2 max-w-xl text-justify">{ABOUT_TEXT}</p></div>
+                </div>
+            </div>
+        </div>
     )
 }
 
