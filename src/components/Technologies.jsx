83d1e17aa11ec1d0403dceef7b91aec912806eda
Supun <Supuntharakaxz@gamil.com>
2024-10-07 11:35:48 +0530

    technologies section animation

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -14,12 +14,12 @@ function Technologies() {
         <div className="border-b border-white pb-24">
             <h1 className="my-20 text-center text-4xl">Technologies</h1>
 
+            {/* --01 */}
             <div className="flex flex-wrap items-center justify-center gap-4">
-
                 <motion.div
                     animate={{ y: [0, -2, 0, 2, 0] }}
                     transition={{
-                        duration: 1.5,
+                        duration: 2,
                         repeat: Infinity,
                         repeatType: "loop",
                         delay: 0
@@ -28,33 +28,92 @@ function Technologies() {
                     <RiReactjsLine className="text-6xl text-cyan-400" />
                 </motion.div>
 
-                <div className="rounded-2xl border-purple-700 border-4 p-3">
+                {/* --02 */}
+                <motion.div
+                    animate={{ y: [-2, 0, 2, 0, -2] }}
+                    transition={{
+                        duration: 2,
+                        repeat: Infinity,
+                        repeatType: "loop",
+                        delay: 0
+                    }}
+                    className="rounded-2xl border-purple-700 border-4 p-3">
                     <FaJs className="text-6xl text-yellow-400" />
-                </div>
+                </motion.div>
 
-                <div className="rounded-2xl border-purple-700 border-4 p-3">
+                {/* --03 */}
+                <motion.div
+                    animate={{ y: [0, 2, 0, -2, 0] }}
+                    transition={{
+                        duration: 2,
+                        repeat: Infinity,
+                        repeatType: "loop",
+                        delay: 0
+                    }}
+                    className="rounded-2xl border-purple-700 border-4 p-3">
+
                     <BiLogoMongodb className="text-6xl text-green-400" />
-                </div>
+                </motion.div>
 
-                <div className="rounded-2xl border-purple-700 border-4 p-3">
+                <motion.div
+                    animate={{ y: [0, -2, 0, 2, 0] }}
+                    transition={{
+                        duration: 2,
+                        repeat: Infinity,
+                        repeatType: "loop",
+                        delay: 0
+                    }}
+                    className="rounded-2xl border-purple-700 border-4 p-3">
                     <SiExpress className="text-6xl text-white" />
-                </div>
+                </motion.div>
 
-                <div className="rounded-2xl border-purple-700 border-4 p-3">
+                <motion.div
+                    animate={{ y: [-2, 0, 2, 0, -2] }}
+                    transition={{
+                        duration: 2,
+                        repeat: Infinity,
+                        repeatType: "loop",
+                        delay: 0
+                    }}
+                    className="rounded-2xl border-purple-700 border-4 p-3">
                     <FaNodeJs className="text-6xl text-green-400" />
-                </div>
+                </motion.div>
 
-                <div className="rounded-2xl border-purple-700 border-4 p-3">
+                <motion.div
+                    animate={{ y: [0, 2, 0, -2, 0] }}
+                    transition={{
+                        duration: 2,
+                        repeat: Infinity,
+                        repeatType: "loop",
+                        delay: 0
+                    }}
+                    className="rounded-2xl border-purple-700 border-4 p-3">
                     <RiTailwindCssFill className="text-6xl text-blue-400" />
-                </div>
+                </motion.div>
 
-                <div className="rounded-2xl border-purple-700 border-4 p-3">
+                <motion.div
+                    animate={{ y: [0, -2, 0, 2, 0] }}
+                    transition={{
+                        duration: 2,
+                        repeat: Infinity,
+                        repeatType: "loop",
+                        delay: 0
+                    }}
+                    className="rounded-2xl border-purple-700 border-4 p-3">
                     <FaHtml5 className="text-6xl text-orange-600" />
-                </div>
+                </motion.div>
 
-                <div className="rounded-2xl border-purple-700 border-4 p-3">
+                <motion.div
+                    animate={{ y: [-2, 0, 2, 0, -2] }}
+                    transition={{
+                        duration: 2,
+                        repeat: Infinity,
+                        repeatType: "loop",
+                        delay: 0
+                    }}
+                    className="rounded-2xl border-purple-700 border-4 p-3">
                     <FaBootstrap className="text-6xl text-purple-500" />
-                </div>
+                </motion.div>
 
             </div>
         </div>
