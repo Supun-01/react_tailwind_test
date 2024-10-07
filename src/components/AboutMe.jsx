b775c41bdc2eac25b84ad8fcea06b2d459520177
Supun <Supuntharakaxz@gamil.com>
2024-10-07 10:03:00 +0530

    About section animation

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,5 +1,6 @@
 import AboutImg from "../assets/about.jpg"
 import { ABOUT_TEXT } from "../constants"
+import { motion } from "framer-motion"
 
 function AboutMe() {
     return (
@@ -8,15 +9,25 @@ function AboutMe() {
             </h2>
 
             <div className="flex flex-wrap">
-                <div className="w-full lg:w-1/2 lg:p-8">
+                <motion.div
+                    initial={{ x: -100, opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    transition={{ duration: 0.5 }}
+
+                    className="w-full lg:w-1/2 lg:p-8">
                     <div className="flex items-center justify-center">
                         <img className="rounded-2xl" src={AboutImg} alt="About Image" />
                     </div>
 
-                </div>
-                <div className="w-full lg:w-1/2 lg:p-8">
+                </motion.div>
+
+                <motion.div
+                    initial={{ x: 100, opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    transition={{ duration: 0.5 }}
+                    className="w-full lg:w-1/2 lg:p-8">
                     <div className="flex justify-center lg:justify-start" ><p className="my-2 max-w-xl text-justify">{ABOUT_TEXT}</p></div>
-                </div>
+                </motion.div>
             </div>
         </div>
     )
